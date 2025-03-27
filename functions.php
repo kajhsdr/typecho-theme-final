d4dcfe44a627433f966ec3f2941736db589b556d
molikai-work <165390418+molikai-work@users.noreply.github.com>
2025-03-27 22:26:57 +0800

    添加自定义头部代码设置项并调整设置项提示文本

--- functions.php
+++ functions.php
@@ -14,12 +14,21 @@ function themeConfig($form)
     );
     $form->addInput($logoUrl);
 
+    $addhead = new \Typecho\Widget\Helper\Form\Element\Textarea(
+        "addhead",
+        null,
+        null,
+        _t("头部代码"),
+        _t("可填写自定义 CSS、JS 代码等")
+    );
+    $form->addInput($addhead);
+
     $addfoot = new \Typecho\Widget\Helper\Form\Element\Textarea(
         "addfoot",
         null,
         null,
-        _t("页脚代码,可填写备案,统计等信息"),
-        _t("支持HTML")
+        _t("页脚代码"),
+        _t("支持 HTML，可填写备案、统计等信息")
     );
     $form->addInput($addfoot);
 }
